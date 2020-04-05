c82db4b07e351c88026f021fef2f063adb03d46a
isaac.diaz <gusilight80@gmail.com>
2020-04-05 20:49:07 +0200

    routes updated

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SearchesHomeComponent } from './searches-home/searches-home.component';
+import { DataHomeComponent } from './data-home/data-home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'search',
+    pathMatch: 'full'
+  },
   { path: 'search', component: SearchesHomeComponent },
+  { path: 'data', component: DataHomeComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
