851c0b45f74818a07998301f4d2de3ce9139aeef
isaac.diaz <gusilight80@gmail.com>
2020-04-05 20:49:48 +0200

    data component

---
+++ frontend/src/app/data-home/data-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-home',
+  templateUrl: './data-home.component.html',
+  styleUrls: ['./data-home.component.scss']
+})
+export class DataHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
