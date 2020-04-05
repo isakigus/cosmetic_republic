5c6555b1adbd7a4d2efe6213eaffb2c714a4d08e
isaac.diaz <gusilight80@gmail.com>
2020-04-05 20:49:34 +0200

    search componet updated

--- frontend/src/app/searches-home/searches-home.component.ts
+++ frontend/src/app/searches-home/searches-home.component.ts
@@ -6,6 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./searches-home.component.scss']
 })
 export class SearchesHomeComponent implements OnInit {
+  searchTextPlaceholder = "Empieza a buscar ;)"
+  searchExplanation = ""
+  searchValue = ""
+
+  onSearch(value: String) {
+    this.searchValue = '' + value
+    this.findExplanation()
+  }
+
+  findExplanation() {
+    let elements = this.searchValue.split(',')
+
+    this.searchExplanation = `
+    número de cosas = ${elements.length}
+    <br/>
+   `+ elements.map((item, i) => `<div> cosa #${i} > ${item}</div>`).join('')
+  }
 
   constructor() { }
 
