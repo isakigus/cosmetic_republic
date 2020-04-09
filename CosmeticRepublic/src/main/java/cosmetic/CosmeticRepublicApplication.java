9bd810a31d90a795333da179ec6c1e28e7a2cda8
Loretodpinedo <ororomb@yahoo.es>
2020-04-09 22:12:21 +0100

    Cambios fichero

---
+++ CosmeticRepublic/src/main/java/cosmetic/CosmeticRepublicApplication.java
@@ -0,0 +1,13 @@
+package cosmetic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmeticRepublicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmeticRepublicApplication.class, args);
+	}
+
+}
